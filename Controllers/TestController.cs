a6588cfc8ed0b2fc673d82ba248d6928268b95b3
ahmedmudassar11 <ahmedmudassar011@gmail.com>
2026-07-19 22:23:48 +0500

    RabbitMQ

---
+++ Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly PdfService _pdfService;
+        private readonly EmbeddingService _embeddingService;
+
+        public TestController(
+            PdfService pdfService,
+            EmbeddingService embeddingService)
+        {
+            _pdfService = pdfService;
+            _embeddingService = embeddingService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var text = _pdfService.ReadPdf("Documents/HRPolicy.pdf");
+
+            var embedding = await _embeddingService.GenerateEmbeddingAsync(text);
+
+            return Ok(new
+            {
+                Length = embedding.Length,
+                FirstFiveValues = embedding.Take(5)
+            });
+        }
+    }
+}
